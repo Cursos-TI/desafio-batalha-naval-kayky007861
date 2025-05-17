05415305796f4ac7ecdd62da7b9de39d300dc05f
kayky007861 <c90672358@gmail.com>
2025-05-17 14:53:04 +0000

    Desafio Batalha naval nivel novato concluido.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,39 @@
-#include <stdio.h>
-
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#include <stdio.h> 
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    //BATALHA NAVAL
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    //Inicialização da tabela 
+    int Matriz[10][10] =
+    {
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 0
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 1
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 2
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 3
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 4
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 5
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 6
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 7
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 8
+     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},//linha 9
+     };
+     //Abaixo será usada uma estrutura de Repetição para imprimir a tabela e adcionar os navios
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+    for(int i = 0, j = 0;i < 10; i++)//For usada para criar a tabela 
+    {
+     //Impresão da tabela 
+     printf("[ %d, %d, %d, %d, %d, %d, %d, %d, %d, %d ]\n",
+     Matriz[i][0], Matriz[i][1],  
+     Matriz[i][2], Matriz[i][3],  
+     Matriz[i][4], Matriz[i][5],  
+     Matriz[i][6], Matriz[i][7],
+     Matriz[i][8], Matriz[i][9]); 
+     //Adição dos navios na tabela 
+    for(int j = 0; j < 3; j++)
+     Matriz[2][j]= 3; Matriz[8][j]= 3;
+     Matriz[2][j]= 3; Matriz[7][j]= 3;
+     Matriz[2][j]= 3; Matriz[6][j]= 3;
+    }
+ return 0;//Progrma finalizou com sucesso 
 }
